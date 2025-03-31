6ac7f5caf3d943692bd0ca6eebc9db1494a5b5f0
maxguuse <max.pink.goose@gmail.com>
2025-03-31 12:32:17 +0300

    bunch of changes in hyprland config
    * neovim as manpager
    * keybinds for playerctl and muting microphone
    * minor changes to gaps
    * window rules syntax changer accounted for newest hyprland

--- dot_config/hypr/scripts/executable_change_monitors.sh
+++ dot_config/hypr/scripts/executable_change_monitors.sh
@@ -6,16 +6,13 @@ fi
 
 if [[ "$choice" == "Dual" ]]; then
 	echo "\$config = /home/ggoose/.config/hypr/configs/dual-monitors" > /home/ggoose/.config/hypr/configs/monitors.conf
-	echo "source = \$config/monitors.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
-	echo "source = \$config/workspaces.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
 elif [[ "$choice" == "External" ]]; then
 	echo "\$config = /home/ggoose/.config/hypr/configs/external-monitor" > /home/ggoose/.config/hypr/configs/monitors.conf
-	echo "source = \$config/monitors.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
-	echo "source = \$config/workspaces.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
 elif [[ "$choice" == "Internal" ]]; then
 	echo "\$config = /home/ggoose/.config/hypr/configs/internal-monitor" > /home/ggoose/.config/hypr/configs/monitors.conf
-	echo "source = \$config/monitors.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
-	echo "source = \$config/workspaces.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
 fi
+
+echo "source = \$config/monitors.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
+echo "source = \$config/workspaces.conf" >> /home/ggoose/.config/hypr/configs/monitors.conf
 
 hyprctl reload
